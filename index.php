7d7c4a8dcc1948c8dc3f368c976dcc3f40c8d4b5
鉛華 <admin@yloveo.com>
2018-06-27 19:35:56 +0800

    1
    1

--- index.php
+++ index.php
@@ -11,9 +11,26 @@ $DatabasePort='3306';#数据库端口
 $DatabaseUsername='';#数据库用户名
 $DatabasePassword='';#数据库密码
 $DatabaseName='';#数据库名
-$Connect=($DatabaseHost,$DatabaseUsername,$DatabasePassword,$DatabaseName,$DatabasePort);#数据库连接
+$DatabaseTable='';#数据表名
+$Connect=mysqli_connect($DatabaseHost,$DatabaseUsername,$DatabasePassword,$DatabaseName,$DatabasePort);#数据库连接
 #---构建数据库类
-
+class DATABASE{
+	#---Connect定义
+	var $Connect;
+	function setConnect($Connect){
+		this->Connect=$Connect;
+	}
+	#---Sql语句自定义
+	var $Sql;
+	function setSql(){
+		this->Sql=$Sql;
+	}
+	#--查询结果
+	function result($Sql){
+		$Result=mysqli_query($Connect,$Sql);
+		$Last=mysqli_fetch_array($Result);
+	}
+}
 #---构建API函数类
 class MOTTO{
 	#---传入参数$_GET['style']
